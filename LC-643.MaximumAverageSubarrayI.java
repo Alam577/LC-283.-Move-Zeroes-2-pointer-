d5c63ebc0f9ad87e95c233a3c32693408bdf8006
Alam577 <166747202+Alam577@users.noreply.github.com>
2026-03-14 23:49:25 +0530

    Implement Maximum Average Subarray I solution
    Ramdan and Coding Day 24

---
+++ LC-643.MaximumAverageSubarrayI.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int nums[] = {1,12,-5,-6,50,3};
+        int k = 4;
+
+        int sum = 0;
+
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        for(int i = k; i < nums.length; i++){
+            sum = sum + nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        double result = (double) maxSum / k;
+
+        System.out.println(result);
+    }
+}
